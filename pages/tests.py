878c0e29a32d8203744ee0ef8e642f7fec7f1700
Douglas Barbosa <douglas.makubex@gmail.com>
2018-03-01 16:56:00 -0300

    added tests

--- pages/tests.py
+++ pages/tests.py
@@ -1,10 +1,38 @@
-from django.test import SimpleTestCase
+from django.test import TestCase
+from django.urls import reverse
+from .models import Promotora
 
-class PagesTests(SimpleTestCase):
-	def test_home_page_status_code(self):
-		response = self.client.get('/')
-		self.assertEqual(response.status_code, 200)
+class PromotoraModelTest(TestCase):
 
-	def test_about_page_status_code(self):
-		response = self.client.get('/sobre/')
-		self.assertEqual(response.status_code, 200)
+	def setUp(self):
+		Promotora.objects.create(nome="Katia", endereco="rua",
+								rg="1",
+								cpf="2",
+								ctps="3")
+
+	def test_fields_content(self):
+		promotora = Promotora.objects.get(id=1)
+		expected_object_name = f'{promotora.nome}', f'{promotora.endereco}', f'{promotora.rg}', f'{promotora.cpf}', f'{promotora.ctps}'
+		self.assertEqual(expected_object_name, ('Katia', 'rua', "1", "2", "3"))
+
+
+class HomePageViewTest(TestCase):
+
+	def setUp(self):
+		Promotora.objects.create(nome="Katia", endereco="rua",
+								rg="1",
+								cpf="2",
+								ctps="3")
+		
+	def test_view_url_exists_at_proper_location(self):
+		resp = self.client.get('/')
+		self.assertEqual(resp.status_code, 200)
+
+	def test_view_url_by_name(self):
+		resp = self.client.get(reverse('home'))
+		self.assertEqual(resp.status_code, 200)
+
+	def test_view_uses_correct_template(self):
+		resp = self.client.get(reverse('home'))
+		self.assertEqual(resp.status_code, 200)
+		self.assertTemplateUsed(resp, 'home.html')
